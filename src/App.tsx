8a0cfc51b3f38523ca23032322a83b087fc63cc8
Gyeop <yanggieop@gmail.com>
2021-02-17 23:35:42 +0900

    Setup react-native-bootsplash library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet, TouchableOpacity, View} from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 import CustomText from './components/CustomText';
 
 const styles = StyleSheet.create({
@@ -8,12 +9,26 @@ const styles = StyleSheet.create({
 });
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  }, []);
+
+  const handlePressButton = () => {
+    RNBootSplash.show({fade: true});
+
+    setTimeout(() => {
+      RNBootSplash.hide({fade: true});
+    }, 2000);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.flexBox}>
-        <CustomText center fontSize={14}>
-          React Native Playground
-        </CustomText>
+        <TouchableOpacity onPress={handlePressButton}>
+          <CustomText center fontSize={14}>
+            {'스플래시 스크린 열기'}
+          </CustomText>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
