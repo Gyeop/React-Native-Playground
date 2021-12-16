630d0f722c238fd0d826d5bee90097a21e239533
Gyeop <yanggieop@gmail.com>
2021-12-16 15:43:46 +0900

    Support Xcode 13.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import CustomText from './components/CustomText';
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
-  flexBox: {flex: 1, alignContent: 'center', justifyContent: 'center'},
+  container: { flex: 1 },
+  flexBox: { flex: 1, alignContent: 'center', justifyContent: 'center' },
 });
 
 const App = () => {
